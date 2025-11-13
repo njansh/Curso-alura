59c557d566aea0038f3ad1f1144e71cd844f3311
njansh <nadsonjhonyobmep@gmail.com>
2025-11-13 19:40:25 -0300

    desafio 1 do modulo 2

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,15 +1,17 @@
+import br.com.nadson.modelo.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme=new Filme();
-        meuFilme.nome="o poderoso Chefao";
-        meuFilme.anoDeLancamento=1970;
-        meuFilme.duracaoEmMinutos=80;
-        meuFilme.incluidoNoPlano=true;
+        meuFilme.setNome("o poderoso Chefao");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(80);
+        meuFilme.setIncluidoNoPlano(true);
         meuFilme.avalia(10);
         meuFilme.avalia(10);
         meuFilme.avalia(10);
 
-        System.out.println("A media de notas foi "+meuFilme.pegaMedia()+" Com "+meuFilme.totalDeAvaliacoes+" Avaliações");
+        System.out.println("A media de notas foi "+meuFilme.pegaMedia()+" Com "+ meuFilme.getTotalDeAvaliacoes() +" Avaliações");
 
 
                 }
