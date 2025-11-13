9d02961539d6ff9bff67ac251960b78aa5d2a7e5
njansh <nadsonjhonyobmep@gmail.com>
2025-11-13 19:02:26 -0300

    finalizando o modulo 1 de java com orientações a objetos

--- desafio/src/contabancaria/ContaBancaria.java
+++ desafio/src/contabancaria/ContaBancaria.java
@@ -1,4 +1,4 @@
-package desafios;
+package contabancaria;
 
 import java.util.Scanner;
 
---
+++ desafio/src/desafios/java_oo_m1/Pessoa.java
@@ -0,0 +1,16 @@
+package desafios.java_oo_m1;
+
+public class Pessoa {
+
+    void olaMundo(){
+        System.out.println("Ola mundo");
+
+    }
+
+}
+class Principal1{
+    public static void main(String[] args) {
+        Pessoa pessoa1=new Pessoa();
+        pessoa1.olaMundo();
+    }
+}
