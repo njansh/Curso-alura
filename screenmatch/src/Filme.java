b75c2441ec39f275df9ca8618a48e076c285f5fa
njansh <nadsonjhonyobmep@gmail.com>
2025-11-13 05:12:21 -0300

    finalizando a aula

--- screenmatch/src/Filme.java
+++ screenmatch/src/Filme.java
@@ -1,2 +1,23 @@
 public class Filme {
+    String nome;
+    int anoDeLancamento;
+    double somaDasAvaliacoes;
+    boolean incluidoNoPlano;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("o nome do filme é "+nome);
+        System.out.println("Ao ano de lancamento é "+anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes+=nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        double media=(somaDasAvaliacoes/totalDeAvaliacoes);
+        return media;
+    }
 }
---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme=new Filme();
+        meuFilme.nome="o poderoso Chefao";
+        meuFilme.anoDeLancamento=1970;
+        meuFilme.duracaoEmMinutos=80;
+        meuFilme.incluidoNoPlano=true;
+        meuFilme.avalia(66);
+        meuFilme.avalia(88);
+
+
+                }
+
+
+
+    }
