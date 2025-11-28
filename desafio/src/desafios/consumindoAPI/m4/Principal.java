9111495daabb068e3305de59feae3b7be81c2f32
njansh <nadsonjhonyobmep@gmail.com>
2025-11-28 18:01:44 -0300

    desafio modulo 4 de Consulta em api

---
+++ desafio/src/desafios/consumindoAPI/m4/Principal.java
@@ -0,0 +1,8 @@
+package desafios.consumindoAPI.m4;
+
+public class Principal {
+    public static void main(String[] args) {
+        new Desafio1();
+
+    }
+}
--- desafio/src/desafios/list_e_colections/m4/Titulo.java
+++ desafio/src/desafios/list_e_colections/m4/Titulo.java
@@ -3,14 +3,34 @@ package desafios.list_e_colections.m4;
 
 public class Titulo implements Comparable <Titulo>{
     private String nome;
+    private double duracao;
+private double nota;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 
     public Titulo(String nome) {
         this.nome = nome;
     }
 
-    public String getNome() {
-        return nome;
-    }
+
 
     @Override
     public int compareTo(Titulo o) {
