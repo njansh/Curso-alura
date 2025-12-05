8e9d7ba82f5a6ac03ae324341cc91605084e0405
njansh <nadsonjhonyobmep@gmail.com>
2025-12-05 17:51:58 -0300

    Exercicos 3 do curso 6 de praticando java

---
+++ praticando_java/src/br/com/nadson/desafiosc6/principal/AcessandoElementos.java
@@ -0,0 +1,20 @@
+package br.com.nadson.desafiosc6.principal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AcessandoElementos {
+    public static void main(String[] args) {
+        List<String> nomes=new ArrayList<>();
+
+        nomes.add("João");
+        nomes.add("Maria");
+        nomes.add("Pedro");
+        nomes.add("Vitoria");
+        nomes.add("Nadson");
+
+        System.out.println("Nome do indice 2: " + nomes.get());
+        System.out.println("Total de nomes na lista: "+nomes.size());
+    }
+
+}
