497ff8ab23d3906975e6ab128dffd020da043787
njansh <nadsonjhonyobmep@gmail.com>
2025-12-07 06:50:53 -0300

    Adiciona mensagem de confirmação ao cadastrar veículos no programa principal.
    - Inclui `System.out.println("Carro cadastrado:")` após criar cada carro
    - Melhora a clareza do fluxo de cadastro de veículos
    - Facilita a identificação de cada cadastro no console
    - Não altera a lógica de criação dos objetos Carro

--- praticando_java/src/br/com/nadson/desafiosc8/principal/CadastrandoVeiculos.java
+++ praticando_java/src/br/com/nadson/desafiosc8/principal/CadastrandoVeiculos.java
@@ -6,12 +6,12 @@ public class CadastrandoVeiculos {
 
 	public static void main(String[] args) {
 Carro carro1 = new Carro("Toyota Corolla", "ABC-1234", 2020);
-
+System.out.println("Carro cadastrado:");
 System.out.println(carro1.getModelo());
 System.out.println(carro1.getPlaca());
 System.out.println(carro1.getAno());
 Carro carro2 = new Carro("Honda Civic", "DEF-5678", 2019);
-
+System.out.println("Carro cadastrado:");
 System.out.println(carro2.getModelo());
 System.out.println(carro2.getPlaca());
 System.out.println(carro2.getAno());
