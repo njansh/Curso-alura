73dfc734d1478d31fea6b44a5a70109cb01fffbb
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 10:25:56 -0300

    feat: adiciona classe Assinatura para sistema de avaliações

--- desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Assinatura.java
+++ desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Assinatura.java
@@ -1,4 +1,32 @@
 package br.com.nadson.avaliacao.model;
 
 public class Assinatura {
+    private static Long INCREMENT_ID = 0l;
+    private final Long id;
+    private String tipo;
+
+    public Assinatura() {
+        this.id = INCREMENT_ID++;
+    }
+
+    public Assinatura(String tipo) {
+        this.id = INCREMENT_ID++;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Assinatura{" +
+               "id=" + id +
+               ", tipo='" + tipo + '\'' +
+               '}';
+    }
 }
