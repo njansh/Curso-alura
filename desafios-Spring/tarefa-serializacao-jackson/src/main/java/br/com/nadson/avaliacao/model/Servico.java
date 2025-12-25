08bcd187f46c9218a28bd622e6f29fe60a2c5663
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 10:25:33 -0300

    feat: adiciona classe Servico para sistema de avaliações

--- desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Servico.java
+++ desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Servico.java
@@ -1,4 +1,32 @@
 package br.com.nadson.avaliacao.model;
 
 public class Servico {
+    private static Long INCREMENT_ID = 0l;
+    private final Long id;
+    private String descricao;
+
+    public Servico() {
+        this.id = INCREMENT_ID++;
+    }
+
+    public Servico(String descricao) {
+        this.id = INCREMENT_ID++;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Servico{" +
+               "id=" + id +
+               ", nome='" + descricao + '\'' +
+               '}';
+    }
 }
