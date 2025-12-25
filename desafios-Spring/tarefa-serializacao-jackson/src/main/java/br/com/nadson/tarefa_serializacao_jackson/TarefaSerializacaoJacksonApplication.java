c5c315a2ad31bdf1f8d378a3a63528c51e6cf888
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 09:53:46 -0300

    feat: cria projeto Spring Boot para serialização com Jackson

---
+++ desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/tarefa_serializacao_jackson/TarefaSerializacaoJacksonApplication.java
@@ -0,0 +1,13 @@
+package br.com.nadson.tarefa_serializacao_jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaSerializacaoJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaSerializacaoJacksonApplication.class, args);
+	}
+
+}
---
+++ desafios-Spring/tarefa-serializacao-jackson/src/test/java/br/com/nadson/tarefa_serializacao_jackson/TarefaSerializacaoJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nadson.tarefa_serializacao_jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaSerializacaoJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
