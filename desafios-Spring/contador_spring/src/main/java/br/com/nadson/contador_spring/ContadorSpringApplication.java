6c4cb52d2521d08c1012f59b3eb716cae5cc2a34
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 09:39:13 -0300

    feat: implementa contador via linha de comando com Spring Boot

--- desafios-Spring/contador_spring/src/main/java/br/com/nadson/contador_spring/ContadorSpringApplication.java
+++ desafios-Spring/contador_spring/src/main/java/br/com/nadson/contador_spring/ContadorSpringApplication.java
@@ -1,13 +1,39 @@
 package br.com.nadson.contador_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class ContadorSpringApplication {
+public class ContadorSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContadorSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Digite um número: ");
+
+			if (!scanner.hasNextInt()) {
+				System.out.println("Entrada inválida. Digite um número inteiro.");
+				return;
+			}
+
+			int numero = scanner.nextInt();
+
+			if (numero < 1) {
+				System.out.println("O número deve ser maior que zero.");
+				return;
+			}
+
+			for (int i = 1; i <= numero; i++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
 }
