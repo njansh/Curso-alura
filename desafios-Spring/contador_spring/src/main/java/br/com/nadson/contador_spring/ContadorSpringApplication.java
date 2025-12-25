b31e6fc71e36e8c24f2e9305c89d414fc8f270fe
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 09:03:27 -0300

    feat: cria projeto contador em Spring Boot para desafio do módulo 1

---
+++ desafios-Spring/contador_spring/src/main/java/br/com/nadson/contador_spring/ContadorSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.nadson.contador_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorSpringApplication.class, args);
+	}
+
+}
---
+++ desafios-Spring/contador_spring/src/test/java/br/com/nadson/contador_spring/ContadorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nadson.contador_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
