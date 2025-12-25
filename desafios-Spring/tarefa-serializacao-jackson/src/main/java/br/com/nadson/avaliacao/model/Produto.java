3655049363de6ae7f6ebd3dad3f6401ed925fd9a
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 10:25:11 -0300

    feat: adiciona classe Produto para sistema de avaliações

--- desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Produto.java
+++ desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Produto.java
@@ -1,4 +1,33 @@
 package br.com.nadson.avaliacao.model;
 
 public class Produto {
+    private static Long INCREMENT_ID = 0l;
+    private final Long id;
+    private String nome;
+
+    public Produto() {
+        this.id = INCREMENT_ID++;
+    }
+
+    public Produto( String nome) {
+        this.id = INCREMENT_ID++;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+               "id=" + id +
+               ", nome='" + nome + '\'' +
+               '}';
+    }
 }
