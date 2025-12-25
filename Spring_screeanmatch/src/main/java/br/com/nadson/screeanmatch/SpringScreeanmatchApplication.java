cb326c3f2aceadc22e40879fcf2c8dca26fc1555
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 11:53:45 -0300

    feat(main): implementa fluxo de consulta para série, episódio isolado e temporada

--- Spring_screeanmatch/src/main/java/br/com/nadson/screeanmatch/SpringScreeanmatchApplication.java
+++ Spring_screeanmatch/src/main/java/br/com/nadson/screeanmatch/SpringScreeanmatchApplication.java
@@ -2,6 +2,7 @@ package br.com.nadson.screeanmatch;
 
 import br.com.nadson.screeanmatch.model.DadosEpisodio;
 import br.com.nadson.screeanmatch.model.DadosSerie;
+import br.com.nadson.screeanmatch.model.DadosTemporada;
 import br.com.nadson.screeanmatch.service.ConsumoApi;
 import br.com.nadson.screeanmatch.service.ConverterDados;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +24,19 @@ public class SpringScreeanmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
+		System.out.println("buscando uma serie completa");
 		String json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls");
 		System.out.println(json);
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
-
+		System.out.println("buscando um episodio isolado");
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=1");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+		System.out.println("buscando uma temporada completa");
+		json=consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1");
+		DadosTemporada dadosTemporada=conversor.obterDados(json,DadosTemporada.class);
+		System.out.println(dadosTemporada);
+
 	}
 }
