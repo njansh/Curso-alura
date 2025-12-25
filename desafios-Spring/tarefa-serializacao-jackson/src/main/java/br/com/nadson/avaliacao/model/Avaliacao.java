9ff7e2d37100f16634d3f49d614ebc08f1bd86f4
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-25 10:22:03 -0300

    feat: cria classe genérica Avaliacao com cálculo de média

---
+++ desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Avaliacao.java
@@ -0,0 +1,41 @@
+package br.com.nadson.avaliacao;
+
+import java.util.List;
+
+public class Avaliacao <T>{
+private T item;
+private double avaliacao;
+private String comentario;
+
+    public Avaliacao(T item, double avaliacao, String comentario) {
+        this.item = item;
+        this.avaliacao = avaliacao;
+        this.comentario = comentario;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+public static <T> double mediaAvaliacoes(List<Avaliacao<T>> avaliacoes){
+        return avaliacoes.stream().mapToDouble(Avaliacao::getAvaliacao).average().orElse(0.0);
+}
+
+
+    @Override
+    public String toString() {
+        return "Avaliacao{" +
+               "item=" + item +
+               ", avaliacao=" + avaliacao +
+               ", comentario='" + comentario + '\'' +
+               '}';
+    }
+}
---
+++ desafios-Spring/tarefa-serializacao-jackson/src/main/java/br/com/nadson/avaliacao/model/Servico.java
@@ -0,0 +1,4 @@
+package br.com.nadson.avaliacao.model;
+
+public class Servico {
+}
