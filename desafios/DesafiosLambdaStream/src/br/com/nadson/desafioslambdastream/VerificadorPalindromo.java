518fd9de9c8f06faf47c8c34b8cafe0b36976f29
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:16:29 -0300

    feat: implementa interface funcional e lógica de palíndromo com StringBuilder

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/VerificadorPalindromo.java
@@ -0,0 +1,5 @@
+package br.com.nadson.desafioslambdastream;
+
+public interface VerificadorPalindromo {
+    boolean verificarPalindromo(String str);
+}
---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/VerificadorPalindromoLambda.java
@@ -0,0 +1,22 @@
+package br.com.nadson.desafioslambdastream;
+
+public class VerificadorPalindromoLambda {
+    public static void main(String[] args) {
+        VerificadorPalindromo verificador = s -> {
+            String invertida = new StringBuilder(s).reverse().toString();
+       return s.equalsIgnoreCase(invertida);
+        };
+        VerificadorPalindromo simplificado = s -> s.equalsIgnoreCase(new StringBuilder(s).reverse().toString());
+
+        String palavra1 = "arara";
+        String palavra2 = "hello";
+
+        System.out.println("Usando 'verificador':");
+        System.out.println("A palavra '" + palavra1 + "' é um palíndromo? " + verificador.verificarPalindromo(palavra1));
+        System.out.println("A palavra '" + palavra2 + "' é um palíndromo? " + verificador.verificarPalindromo(palavra2));
+
+        System.out.println("\nUsando 'simplificado':");
+        System.out.println("A palavra '" + palavra1 + "' é um palíndromo? " + simplificado.verificarPalindromo(palavra1));
+        System.out.println("A palavra '" + palavra2 + "' é um palíndromo? " + simplificado.verificarPalindromo(palavra2));
+    }
+}
