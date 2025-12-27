767642807500b9da91865a5c6352e5a39d1ea290
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:33:42 -0300

    feat: implementa pipeline de Stream com filter e map para processar strings

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/ProcessadorStringsLambda.java
@@ -0,0 +1,17 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+public class ProcessadorStringsLambda {
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("java", "lambda", "stream", "programacao", "funcional");
+        Predicate<String>maisDe3=s -> s.length()>=5;
+        UnaryOperator<String>paraMaiusculo=s -> s.toUpperCase();
+        palavras.stream().filter(maisDe3).map(paraMaiusculo).forEach(System.out::println);
+    }
+
+}
