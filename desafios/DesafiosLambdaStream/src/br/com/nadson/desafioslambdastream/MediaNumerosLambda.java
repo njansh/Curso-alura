ac1863d0a5bb1ee9a12f3e4826cefd368cb464f9
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:51:06 -0300

    feat: implementa cálculo de média com mapToInt e average

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/MediaNumerosLambda.java
@@ -0,0 +1,17 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MediaNumerosLambda {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        double media = numeros.stream()
+                .mapToInt(n -> n) 
+                .average()
+                .orElse(0.0);
+
+        System.out.println("A média é: " + media);
+    }
+}
