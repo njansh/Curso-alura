8c799238a26c366ecce76ad6babe2ff50ab0b22e
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:29:31 -0300

    feat: implementa FiltradorNomes usando Stream, filter e Predicate separado

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/FiltradorNomesLambda.java
@@ -0,0 +1,14 @@
+    package br.com.nadson.desafioslambdastream;
+
+    import java.util.Arrays;
+    import java.util.List;
+    import java.util.function.Predicate;
+    import java.util.stream.Collectors;
+
+    public class FiltradorNomesLambda {
+        public static void main(String[] args) {
+            List<String> nomes = Arrays.asList("Alice", "Bob", "Ana", "Carlos", "Amanda");
+            Predicate<String> iniciaComA = nome -> nome.startsWith("A");
+            nomes.stream().filter(iniciaComA).forEach(System.out::println);
+        }
+    }
