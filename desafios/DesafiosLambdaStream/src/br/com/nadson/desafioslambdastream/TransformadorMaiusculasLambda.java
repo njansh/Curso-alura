f4f3d9a83802bbe611e3235fb87b13cc9e1d2463
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:07:58 -0300

    feat: implementa UnaryOperator com lambda para converter string em maiúsculas

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/TransformadorMaiusculasLambda.java
@@ -0,0 +1,11 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.function.UnaryOperator;
+
+public class TransformadorMaiusculasLambda {
+    public static void main(String[] args) {
+        UnaryOperator<String> paraMaiuscula=s->s.toUpperCase();
+        String nome="Estou aqui";
+        System.out.println("Nome em maiúsculas: " + paraMaiuscula.apply(nome));
+    }
+}
