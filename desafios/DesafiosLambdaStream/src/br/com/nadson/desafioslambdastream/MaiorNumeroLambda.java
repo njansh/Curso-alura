7af37c9f452cf470af51e4cab433b0f921c30443
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:47:40 -0300

    feat: implementa busca pelo maior número usando reduce e Integer::max

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/MaiorNumeroLambda.java
@@ -0,0 +1,17 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class MaiorNumeroLambda {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(10, 5, 8, 20, 15);
+        
+        BinaryOperator<Integer> maiorNumero = (a, b) -> Integer.max(a, b);
+        
+        int resultado = numeros.stream()
+                               .reduce(Integer.MIN_VALUE, maiorNumero);
+        System.out.println("O maior número na lista é: " + resultado);
+    }
+}
