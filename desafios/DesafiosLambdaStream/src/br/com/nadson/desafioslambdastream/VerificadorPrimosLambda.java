f48029d6a94fa0a8c494344c5aa66d41868068df
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:02:02 -0300

    feat: implementa Predicate com lambda para verificar números primos

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/VerificadorPrimosLambda.java
@@ -0,0 +1,23 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.function.Predicate;
+
+public class VerificadorPrimosLambda {
+    public static void main(String[] args) {
+        Predicate<Integer> isPrimo = n -> {
+            if (n<=1){
+                return false;
+            }
+            for (int i = 2; i <=    n/2 ; i++) {
+                if(n%i==0){
+                    return false;
+                }
+
+            }
+            return true;
+        };
+
+        int numeroParaTeste = 4;
+        System.out.println("O número " + numeroParaTeste + " é primo? " + isPrimo.test(numeroParaTeste));
+    }
+}
