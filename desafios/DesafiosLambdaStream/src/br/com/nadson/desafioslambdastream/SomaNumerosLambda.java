be605539c2dc9234f4c6a5adcfad424c9f5f2e2e
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 20:42:49 -0300

    feat: implementa soma de lista com reduce e identidade 0

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/SomaNumerosLambda.java
@@ -0,0 +1,18 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class SomaNumerosLambda {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(10, 20, 30, 40);
+
+        BinaryOperator<Integer> soma = (a, b) -> a + b;
+
+        int resultado = numeros.stream()
+                .reduce(0, soma);
+
+        System.out.println("A soma total é: " + resultado);
+    }
+}
