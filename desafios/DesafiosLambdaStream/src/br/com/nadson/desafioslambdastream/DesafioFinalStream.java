ffe78366a50baaa8cff4495d52f12b91003e4fbc
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 21:00:41 -0300

    feat: implementa desafio final combinando filter, map e reduce

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/DesafioFinalStream.java
@@ -0,0 +1,20 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+public class DesafioFinalStream {
+    public static void main(String[] args) {
+        List<Double> precos = Arrays.asList(5.0, 15.0, 20.0, 8.0, 30.0);
+        Predicate<Double> maiorQue10= n->n>=10;
+        UnaryOperator<Double>descont10=n->n*0.9;
+        BinaryOperator<Double>somar=(a,b)->a+b;
+        
+        precos.stream().filter(maiorQue10).map(descont10).reduce(somar).ifPresent(
+                total -> System.out.println("Soma dos preços com desconto: " + total)
+        );
+    }
+}
