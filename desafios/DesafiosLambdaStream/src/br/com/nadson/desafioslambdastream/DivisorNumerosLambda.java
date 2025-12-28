de1ade2c1fa0a8696b33d7f31d40eb1f5fb547b1
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-27 21:10:15 -0300

    feat: implementa divisor com tratamento de erro usando BiFunction

---
+++ desafios/DesafiosLambdaStream/src/br/com/nadson/desafioslambdastream/DivisorNumerosLambda.java
@@ -0,0 +1,19 @@
+package br.com.nadson.desafioslambdastream;
+
+import java.util.function.BiFunction;
+
+public class DivisorNumerosLambda {
+    public static void main(String[] args) {
+        BiFunction<Double,Double,Double>divisao=(a,b)->{
+          if(b==0){
+              throw new ArithmeticException("Não é possível dividir por zero");
+          }
+
+
+            return a/b;
+        };
+
+        double resultado = divisao.apply(10.0, 2.0);
+        System.out.println("O resultado da divisão é: " + resultado);
+    }
+}
